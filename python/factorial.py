2100d5dd2d30c787b45aa3ff4210ccdab07d97b7
ashish-1221 <maharanaashish72@gmail.com>
2023-07-04 23:13:42 +0530

    Factorial of a number

--- python/factorial.py
+++ python/factorial.py
@@ -1,10 +1,11 @@
 """Function to generate factorial of a number"""
 def fact(num):
-    sum=1
+    """Function to print factorial of a number"""
+    sum1=1
     while(num>0):
-        sum = sum*num
+        sum1 = sum1*num
         num-=1
-    return sum
+    return sum1
 if __name__=="__main__":
     print(fact(5))
     
